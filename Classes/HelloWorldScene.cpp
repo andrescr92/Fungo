2fcacc1fce16b569ad02025d3c5fea97b87ee9d2
aivaco <aivan031092@hotmail.com>
2014-04-20 14:06:22 -0600

    Pequeños cambios menú del juego v1.0

--- Classes/HelloWorldScene.cpp
+++ Classes/HelloWorldScene.cpp
@@ -55,7 +55,7 @@ bool HelloWorld::init()
     // add a label shows "Hello World"
     // create and initialize a label
     
-    auto label = LabelTTF::create("Mientras", "Arial", 24);
+    auto label = LabelTTF::create("", "Arial", 24);
 	
 
     // position the label on the center of the screen
