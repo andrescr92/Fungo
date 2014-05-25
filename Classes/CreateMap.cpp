e0eebfacc5bcbd32b926d0887d614fb7ed510ddf
aivaco <aivan031092@hotmail.com>
2014-05-24 20:39:17 -0600

    Cambios a la clase mapa (nuevos métodos)

--- Classes/CreateMap.cpp
+++ Classes/CreateMap.cpp
@@ -1,11 +1,18 @@
 #include "CreateMap.h"
 #include "HelloWorldScene.h"
 
+//#include "renderer/CCRenderer.h"
+//#include "ui/CocosGUI.h"
+//#include "UISlider.h"
+
+
+
 USING_NS_CC;
+//using namespace ui;
 
 
 
-void CreateMap::crearMapa()
+/*void CreateMap::crearMapa()
 {
 
 	Size visibleSize = Director::getInstance()->getVisibleSize();
@@ -13,11 +20,12 @@ void CreateMap::crearMapa()
 
 	auto map = Sprite::create("mapa.png");
 	map->setPosition(Point(visibleSize.width/2 + origin.x, visibleSize.height/2 + origin.y));
+	map->setOpacityModifyRGB(true);
 	this->addChild(map,0);
 
 
 
-}
+}*/
 
 void CreateMap::createMap()
 {
@@ -25,8 +33,9 @@ void CreateMap::createMap()
 	
 	Size visibleSize = Director::getInstance()->getVisibleSize();
 	Point origin = Director::getInstance()->getVisibleOrigin();
-
+	//Crea el nodo mapa
 	this-> Map = CCTMXTiledMap::create("TiledMaps/mapa6.tmx");
+	//Añade el mapa a la escena en la capa "0"
 	addChild(Map, 0);
 	//Map->setPosition(Point(visibleSize.width/2 + origin.x, visibleSize.height/2 + origin.y));
 	
@@ -34,7 +43,7 @@ void CreateMap::createMap()
 }
 
 
-
+//Encargado de mover al mapa (desplazarlo)
 void CreateMap::moveMap()
 { 
 	Node* Mapita = Map; 
@@ -83,3 +92,30 @@ void CreateMap::moveMap()
 		_eventDispatcher->addEventListenerWithSceneGraphPriority(listener1, Mapita);
 
 }
+
+/*void CreateMap::sizeMap()
+{
+
+	auto size = Director::getInstance()->getWinSize();
+	cocos2d::UISlider* slider = cocos2d::UISlider::create();
+	slider->setTag(1);
+	slider->setTouchEnabled(true);
+	slider->loadBarTexture("sliderTrack.png");
+	slider->loadSlidBallTextures("sliderThumb.png", "sliderThumb.png", "");
+	slider->loadProgressBarTexture("sliderProgress.png");
+	slider->setPosition(Point(size.width / 2.0f, size.height * 0.15f + slider->getSize().height * 2.0f));
+	slider->setPercent(50);
+	//slider->addEventListenerSlider(this, sliderpercentchangedselector(CreateMap::sliderEvent));
+	addChild(slider);
+
+
+
+}
+
+void CreateMap::sliderEvent(){
+
+
+
+
+
+}*/
--- Classes/CreateMap.h
+++ Classes/CreateMap.h
@@ -3,7 +3,12 @@
 #define CreateMap_h
 
 #include "cocos2d.h"
-	
+
+
+/*#include "renderer/CCCustomCommand.h"
+#include "ui/CocosGUI.h"
+#include "UISlider.h"*/
+///Esta clase se encarga de todas las características que posee el modo mapa. Es decir, lo invoca, le permite moverse y le permite escalarse a un tamaño deseado.
 class CreateMap: public cocos2d::Layer
 {
 	friend class HelloWorldScene;
@@ -14,9 +19,13 @@ private:
 
 public:
 	///Método que crea el mapa, es decir lo inicializa como un layer.
-    void crearMapa();
+    //void crearMapa();
 	void createMap();
+	///Procedimiento que permite mover el mapa a través de la pantalla
 	void moveMap();
+	///Método que permite el escalamiento del mapa (cambio de tamaños)
+	void sizeMap();
+	//void sliderEvent();
 
 	CREATE_FUNC(CreateMap);
 };
