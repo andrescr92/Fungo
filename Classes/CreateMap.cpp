a816bd0c01a79eac1c79e6607f66d1561be29dcf
Andres Caballero <andrescr92@gmail.com>
2014-06-19 03:53:20 -0600

    Se implemento el metodo para evitar que el mapa se salga de la pantalla al moverlo por la pantalla. Se utilizaron numeros magicos y presenta un error por lo que debe ser mejorado

--- Classes/CreateMap.cpp
+++ Classes/CreateMap.cpp
@@ -26,7 +26,6 @@ USING_NS_CC;
 
  void CreateMap::createMap()  
 {
-   
 	Size visibleSize = Director::getInstance()->getVisibleSize();
 	Point origin = Director::getInstance()->getVisibleOrigin();
 	//Crea el nodo mapa
@@ -75,6 +74,7 @@ void CreateMap::moveMap()
         //Check the click area
         if (rect.containsPoint(locationInNode))
         {
+			
             log("node began... x = %f, y = %f", locationInNode.x, locationInNode.y);
             target->setOpacity(180);
             return true;
@@ -84,14 +84,159 @@ void CreateMap::moveMap()
 
     //Trigger when moving touch
     listener1->onTouchMoved = [](Touch* touch, Event* event){
+		
         auto target = static_cast<Node*>(event->getCurrentTarget());
         //Move the position of current button sprite
         target->setPosition(target->getPosition() + touch->getDelta());
+
+			//Sin importar el nivel de zoom siempre se cumplen
+			if (target->getPositionX()>0)
+				target->setPositionX(0);
+			if (target->getPositionY()>0)
+				target->setPositionY(0);
+
+			int x = target->getScaleX()*100;
+
+			//Nivel de zoom 1
+			if (x==25){
+				if (target->getPositionY()<-255)
+				target->setPositionY(-255);
+				if (target->getPositionX()<0)
+					target->setPositionX(0);
+			}
+			//Nivel de zoom 2
+			if (x==30){
+				if (target->getPositionY()<-461)
+				target->setPositionY(-461);
+				if (target->getPositionX()<-205)
+					target->setPositionX(-205);
+			}
+
+			//Nivel de zoom 3
+			if (x==35){
+				if (target->getPositionY()<-665)
+				target->setPositionY(-665);
+				if (target->getPositionX()<-410)
+					target->setPositionX(-410);
+			}
+
+			//Nivel de zoom 4
+			if (x==40){
+				if (target->getPositionY()<-870)
+				target->setPositionY(-870);
+				if (target->getPositionX()<-614)
+					target->setPositionX(-614);
+			}
+
+			//Nivel de zoom 5
+			if (x==45){
+				if (target->getPositionY()<-1076)
+				target->setPositionY(-1076);
+				if (target->getPositionX()<-819)
+					target->setPositionX(-819);
+			}
+
+			//Nivel de zoom 6
+			if (x==50){
+				if (target->getPositionY()<-1280)
+				target->setPositionY(-1280);
+				if (target->getPositionX()<-1022)
+					target->setPositionX(-1022);
+			}
+
+			//Nivel de zoom 7
+			if (x==55){
+				if (target->getPositionY()<-1485)
+				target->setPositionY(-1485);
+				if (target->getPositionX()<-1228)
+					target->setPositionX(-1228);
+			}
+
+			//Nivel de zoom 8
+			if (x==60){
+				if (target->getPositionY()<-1690)
+				target->setPositionY(-1690);
+				if (target->getPositionX()<-1433)
+					target->setPositionX(-1433);
+			}
+
+			//Nivel de zoom 9
+			if (x==65){
+				if (target->getPositionY()<-1894)
+				target->setPositionY(-1894);
+				if (target->getPositionX()<-1637)
+					target->setPositionX(-1637);
+			}
+
+			//Nivel de zoom 10
+			if (x==70){
+				if (target->getPositionY()<-2098)
+				target->setPositionY(-2098);
+				if (target->getPositionX()<-1842)
+					target->setPositionX(-1842);
+			}
+
+			//Nivel de zoom 11
+			if (x==75){
+				if (target->getPositionY()<-2304)
+				target->setPositionY(-2304);
+				if (target->getPositionX()<-2047)
+					target->setPositionX(-2047);
+			}
+
+			//Nivel de zoom 12
+			if (x==80){
+				if (target->getPositionY()<-2509)
+				target->setPositionY(-2509);
+				if (target->getPositionX()<-2253)
+					target->setPositionX(-2253);
+			}
+
+			//Nivel de zoom 13
+			if (x==85){
+				if (target->getPositionY()<-2713)
+				target->setPositionY(-2713);
+				if (target->getPositionX()<-2457)
+					target->setPositionX(-2457);
+			}
+
+			//Nivel de zoom 14
+			if (x==90){
+				if (target->getPositionY()<-2918)
+				target->setPositionY(-2918);
+				if (target->getPositionX()<-2663)
+					target->setPositionX(-2663);
+			}
+
+			//Nivel de zoom 15
+			if (x==95){
+				if (target->getPositionY()<-3123)
+				target->setPositionY(-3123);
+				if (target->getPositionX()<-2867)
+					target->setPositionX(-2867);
+			}
+
+			//Nivel de zoom 16
+			if (x==100){
+				if (target->getPositionY()<-3328)
+				target->setPositionY(-3328);
+				if (target->getPositionX()<-3072)
+					target->setPositionX(-3072);
+			}
     };
 
     //Process the touch end event
     listener1->onTouchEnded = [=](Touch* touch, Event* event){
         auto target = static_cast<Node*>(event->getCurrentTarget());
+
+		int x = target->getPositionX();
+		int y = target->getPositionY();
+		log("posicion x = %i, posicion y = %i", x, y);
+
+		float w = target->getScaleX();
+		float z = target->getScaleY();
+		log("escala x = %f, escala y = %f", w, z);
+
         log("node onTouchesEnded.. ");
         target->setOpacity(255);
      
@@ -146,8 +291,7 @@ void CreateMap::increaseSize(Ref * pSender)
 	if (Mapa->getScaleX() < 1.0f && Mapa->getScaleY() < 1.0f){
 		
 		Mapa->setScaleX (Mapa->getScaleX()+0.05f);
-			Mapa->setScaleY (Mapa->getScaleY()+0.05f);
-				 
+		Mapa->setScaleY (Mapa->getScaleY()+0.05f);
 	}
 
 
